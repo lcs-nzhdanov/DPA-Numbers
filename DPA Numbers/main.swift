71ec4c883d4c756378efb559d1c233cbc45a56c3
Nikita Zhdanov <nzhdanov@lcs.on.ca>
2024-09-23 11:32:38 -0400

    Created an input function

--- DPA Numbers/main.swift
+++ DPA Numbers/main.swift
@@ -9,3 +9,37 @@ import Foundation
 
 print("Hello, World!")
 
+
+func getInput(minimum: Int?, maximum: Int?) -> Int {
+    while true {
+        
+        // Read input
+        guard let givenInput = readLine() else {
+            continue
+        }
+        
+        // Check if number is Int
+        guard let givenInteger = Int(givenInput) else {
+            continue
+        }
+        
+        // Compare with lowest limit
+        if let minimumValue = minimum {
+            guard givenInteger >= minimumValue else {
+                continue
+            }
+        }
+        
+        // Compare with highest limit
+        if let maximumValue = maximum {
+            guard givenInteger <= maximumValue else {
+                continue
+            }
+        }
+        
+        // Return input
+        return givenInteger
+    }
+    
+    
+}
